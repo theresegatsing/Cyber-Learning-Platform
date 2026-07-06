942ad395d49c69d81cd177ab427d3ad72dc22d29
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-07-06 16:35:53 -0400

    updated scenario engine

--- frontend/src/engine/scenarioEngine.js
+++ frontend/src/engine/scenarioEngine.js
@@ -2,25 +2,90 @@ export function createScenarioEngine(scenarioData) {
 
     let currentNodeId = scenarioData.start;
 
+    let history = [];
+
 
     function getCurrentNode() {
 
-        return scenarioData.nodes.find(
+        const node = scenarioData.nodes.find(
             node => node.id === currentNodeId
         );
+
+
+        return node;
 
     }
 
 
+
     function goNext(nextNodeId) {
+
+
+        const currentNode = getCurrentNode();
+
+
+        if(currentNode){
+
+            history.push({
+                nodeId: currentNode.id,
+                title: currentNode.title,
+                stage: currentNode.stage
+            });
+
+        }
+
 
         currentNodeId = nextNodeId;
 
     }
 
 
+
+    function getCurrentStage(){
+
+
+        const node = getCurrentNode();
+
+
+        return node?.stage;
+
+    }
+
+
+
+    function getStages(){
+
+        return scenarioData.attack_stages;
+
+    }
+
+
+
+    function getHistory(){
+
+        return history;
+
+    }
+
+
+
+    function getPreventionPoints(){
+
+        return scenarioData.prevention_points;
+
+    }
+
+
+
     return {
+
         getCurrentNode,
-        goNext
+        goNext,
+        getCurrentStage,
+        getStages,
+        getHistory,
+        getPreventionPoints
+
     };
+
 }
