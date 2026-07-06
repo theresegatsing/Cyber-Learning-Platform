1567d9f40683d774b511d0c16eb784599db9eed3
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-07-06 16:25:44 -0400

    updated the backend scenario fetch

--- frontend/src/api/scenarioApi.js
+++ frontend/src/api/scenarioApi.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000";
 
-export async function fetchScenario(system, scenarioId) {
-  const res = await axios.get(
-    `${BASE_URL}/scenarios/${system}/${scenarioId}`
-  );
 
-  return res.data;
+export async function fetchScenario(system, scenarioId) {
+
+    const response = await axios.get(
+        `${BASE_URL}/scenarios/${system}/${scenarioId}`
+    );
+
+    return response.data;
 }
