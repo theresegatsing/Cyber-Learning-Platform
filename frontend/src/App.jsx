fb9c544b9489de91a195cb94d911d758bfe19f6e
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-07-07 16:45:05 -0400

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+/**
+ * ============================================================================
+ * App.jsx
+ * ----------------------------------------------------------------------------
+ * Main entry component of the React application.
+ *
+ * Responsibilities:
+ * - Defines the first page displayed when the application starts.
+ * - Acts as the connection point between different pages.
+ *
+ * Later:
+ * - React Router will be added here.
+ * - Routes will connect Home, ScenarioPlayer, and future pages.
+ * ============================================================================
+ */
+
+
 import Home from "./pages/Home";
 
 
-function App(){
+/**
+ * Root component of the application.
+ */
+export default function App(){
 
-    return <Home/>;
+
+    return (
+
+        <Home />
+
+    );
+
 
 }
-
-export default App;
